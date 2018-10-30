5b7d72f34a79454c0054d8fd60d821e3884dad7c
wendylin <wendylw2013@gmail.com>
2018-10-30 22:58:20 +0800

    Nav: Add script

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 (function() {
     let language = navigator.language;
+
+    $('.mobile-menu').on('click', function() {
+      $('.header__nav').slideDown();
+    });
+
+    $('.header__close-button').on('click', function() {
+      $('.header__nav').slideUp();
+    });
   }());
