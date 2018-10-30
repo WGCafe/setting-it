1641cee89fde41f6876d543724f1db9eb28fa8a9
wendylin <wendylw2013@gmail.com>
2018-10-30 23:14:19 +0800

    Merge main.js

--- js/main.js
+++ js/main.js
@@ -8,8 +8,4 @@
     $('.header__close-button').on('click', function() {
       $('.header__nav').slideUp();
     });
-<<<<<<< HEAD
-  }());
-=======
 }());
->>>>>>> 8cc4fe524fe928bf36ebdd27a2c3e890c37bd807
