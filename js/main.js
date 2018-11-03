19cbe9a4c12616caeec05e1e9c8de96e524dd332
wendylin <wendylw2013@gmail.com>
2018-11-03 22:30:08 +0800

    homepage: Update case modal

--- js/main.js
+++ js/main.js
@@ -9,3 +9,25 @@
       $('.header__nav').slideUp();
     });
 }());
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+          items:1,
+          nav:true
+        }
+    }
+  });
+});
